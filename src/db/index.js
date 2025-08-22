25e49dbc21fecbd6bcbd5d150db102c4fb76182d
Sonu <sonuroy2029@gmail.com>
2025-08-22 12:15:49 +0530

    feat: connect to mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+export default async function connectDB() {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    console.log(`\n MongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
+    
+  } catch (error) {
+    console.log('MongoDB connection error:', error);
+    process.exit(1);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import dotenv from 'dotenv';
+// dotenv.config({ path: './env' });
+// $node -r dotenv/config --experimental-json-modules src/index.js
+import connectDB from "./db/index.js";
+
+
+connectDB();
